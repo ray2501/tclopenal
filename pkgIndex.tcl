f228a48ad42854ee4a2bf9166504a9971cf5b3cd
Danilo Chang <ray2501@gmail.com>
2023-12-15 10:28:04 +0800

    Bump version to 0.7

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded openal 0.6 \
-	    [list load [file join $dir libtcl9openal0.6.so] [string totitle openal]]
+    package ifneeded openal 0.7 \
+	    [list load [file join $dir libtcl9openal0.7.so] [string totitle openal]]
 } else {
-    package ifneeded openal 0.6 \
-	    [list load [file join $dir libopenal0.6.so] [string totitle openal]]
+    package ifneeded openal 0.7 \
+	    [list load [file join $dir libopenal0.7.so] [string totitle openal]]
 }
