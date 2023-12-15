97316ce43ace1c15c5c97bf5c885ad9ed8b4cf53
Danilo Chang <ray2501@gmail.com>
2023-12-15 10:25:12 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded openal 0.6 \
-    [list load [file join $dir libopenal0.6.so] openal]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded openal 0.6 \
+	    [list load [file join $dir libtcl9openal0.6.so] [string totitle openal]]
+} else {
+    package ifneeded openal 0.6 \
+	    [list load [file join $dir libopenal0.6.so] [string totitle openal]]
+}
